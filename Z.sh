f00c9ee50876ae5d8260f0fda6fcaa573ff80c48
marco <mnunom@upv.edu.mx>
2025-05-07 12:14:50 -0600

    Version1746641689

--- Z.sh
+++ Z.sh
@@ -19,9 +19,9 @@ NombreMateria=PresentacionPOO_2024
 NombreMateria=PresentacionSI_2024
 NombreMateria=PresentacionSI_2025
 
-NombreMateria=PresentacionPM_2025
-NombreMateria=PresentacionPI1_2025
 
+NombreMateria=PresentacionPI1_2025
+NombreMateria=PresentacionPM_2025
 
 pdflatex $NombreMateria.tex
 rm *.aux *.log *.nav *.out *.snm *.toc
